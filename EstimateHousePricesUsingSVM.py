21723b6d03b35ed22f6b8cecc7aa6c25c74660f2
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2022-12-22 10:13:27 -0500

    Estimating house prices using SVM

---
+++ EstimateHousePricesUsingSVM.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 22 09:28:59 2022
+
+@author: 15414
+"""
+
+'''We are using a dataset available in sklearn.'''
+
+# Import relevant packages
+import numpy as np
+from sklearn import datasets
+from sklearn.svm import SVR
+from sklearn.utils import shuffle
+from sklearn.metrics import mean_squared_error, explained_variance_score
+
+# Load the housing dataset
+data = datasets.load_boston()
+
+# Shuffle the data to remove our own biases
+X, y = shuffle(data.data, data.target, random_state=7)
+
+# Split the data into training and testing datasets
+num_training = int(0.8 * len(X))
+X_train, y_train = X[:num_training], y[:num_training]
+X_test, y_test = X[num_training:], y[num_training:]
+
+# Create Support Vector Regression model
+sv_regressor = SVR(kernel='linear', C=1.0, epsilon=0.1)
+
+# Train SVR
+sv_regressor.fit(X_train, y_train)
+
+# Evaluate performance of SVR
+y_test_pred = sv_regressor.predict(X_test)
+mse = mean_squared_error(y_test, y_test_pred)
+evs = explained_variance_score(y_test, y_test_pred)
+print("\n#### Performance ####")
+print("Mean squared error =", round(mse, 2))
+print("Explained variance score =", round(evs, 2))
+
+# Test the regressor on a test datapoint
+test_data = [3.7, 0, 18.4, 1, 0.87, 5.95, 91, 2.5052, 26, 666, 20.2, 351.34, 15.27]
+print("\nPredicted price:", sv_regressor.predict([test_data])[0])
