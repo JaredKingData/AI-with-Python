ae34d382eb12067837b785708331982a7ce245bd
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2022-12-22 11:36:35 -0500

    Building a Decision Tree Classifier

---
+++ BuildingDecisionTreeClassifier.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 22 10:51:53 2022
+
+@author: 15414
+"""
+
+# Import relevant packages
+import numpy as np
+import matplotlib.pyplot as plt
+#from sklearn import cross_validation
+from sklearn import model_selection # use this instead of cross_validation (no longer available)
+from sklearn.tree import DecisionTreeClassifier
+
+from utilities import visualize_classifier
+from sklearn.metrics import classification_report
+
+'''We will be using data in the ''data_decision_trees.txt' file. The first two values are input data, and the third is a target label.'''
+
+# Load input data
+input_file = 'data_decision_trees.txt'
+data = np.loadtxt(input_file, delimiter=',')
+X, y = data[:, :-1], data[:, -1]
+
+# Separate the input data into two classes based on labels
+class_0 = np.array(X[y==0])
+class_1 = np.array(X[y==1])
+
+# Vidualize input data
+plt.figure()
+plt.scatter(class_0[:, 0], class_0[:, 1], s=75, facecolors='black', edgecolors='black', linewidth=1, marker='x')
+plt.scatter(class_1[:, 0], class_1[:, 1], s=75, facecolors='white', edgecolors='black', linewidth=1, marker='o')
+plt.title('Input data')
+
+# Split the data into training and testing datasets
+#X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25, random_state=5)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.25, random_state=5)
+
+# Decision trees classifier
+params = {'random_state': 0, 'max_depth': 4}
+classifier = DecisionTreeClassifier(**params)
+classifier.fit(X_train, y_train)
+visualize_classifier(classifier, X_train, y_train)
+
+# Compute output of classifier on the test dataset
+y_test_pred = classifier.predict(X_test)
+visualize_classifier(classifier, X_test, y_test)
+
+# Evaluate classifier performance
+class_names = ['Class-0', 'Class-1']
+print("\n" + "#"*40)
+print("\nClassifier performance on training dataset\n")
+print(classification_report(y_train, classifier.predict(X_train), target_names=class_names))
+print("#"*40 + "\n")
+print("#"*40)
+print("\nClassifier performance on test dataset\n")
+print(classification_report(y_test, y_test_pred, target_names=class_names))
+print("#"*40 + "\n")
+
+plt.show()
+
+'''RESULT
+Classifier performance on test dataset
+
+              precision    recall  f1-score   support
+
+     Class-0       0.93      1.00      0.97        43
+     Class-1       1.00      0.94      0.97        47
+
+    accuracy                           0.97        90
+   macro avg       0.97      0.97      0.97        90
+weighted avg       0.97      0.97      0.97        90
+
+'''
